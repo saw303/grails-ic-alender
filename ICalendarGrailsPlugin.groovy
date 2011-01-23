6a1d3faf55ec880d33e5cab32bae8526581dcf19
Silvio Wangler <silvio.wangler@gmail.com>
2011-01-23 19:48:48 +0000

    Set version to 0.4

--- ICalendarGrailsPlugin.groovy
+++ ICalendarGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import org.codehaus.groovy.grails.commons.DefaultGrailsControllerClass
  */
 class ICalendarGrailsPlugin {
   // the plugin version
-  def version = '0.3'
+  def version = '0.4'
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = '1.3.6 > *'
   // the other plugins this plugin depends on
