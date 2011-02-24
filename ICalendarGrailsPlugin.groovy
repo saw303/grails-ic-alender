0edfcccbd1c9c95bc34e25507b44d519af4b1158
Silvio Wangler <silvio.wangler@gmail.com>
2011-02-24 20:53:17 +0000

    Bugfix version 0.3.1

--- ICalendarGrailsPlugin.groovy
+++ ICalendarGrailsPlugin.groovy
@@ -1,5 +1,4 @@
 import ch.silviowangler.groovy.util.builder.ICalendarBuilder
-import grails.util.GrailsUtil
 import org.codehaus.groovy.grails.commons.DefaultGrailsControllerClass
 
 /*
@@ -22,92 +21,92 @@ import org.codehaus.groovy.grails.commons.DefaultGrailsControllerClass
  * @author Silvio Wangler (silvio.wangler@gmail.com)
  */
 class ICalendarGrailsPlugin {
-  // the plugin version
-  def version = '0.4'
-  // the version or versions of Grails the plugin is designed for
-  def grailsVersion = '1.3.6 > *'
-  // the other plugins this plugin depends on
-  def dependsOn = [controllers: GrailsUtil.grailsVersion]
-  def loadAfter = ['controllers']
-  def observe = ['controllers']
-  // resources that are excluded from plugin packaging
-  def pluginExcludes = [
-          'grails-app/views/error.gsp',
-          'grails-app/controllers/TestController.groovy'
-  ]
+    // the plugin version
+    def version = '0.3.1'
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = '1.1 > *'
+    // the other plugins this plugin depends on
+    def dependsOn = [controllers: '1.1 > *']
+    def loadAfter = ['controllers']
+    def observe = ['controllers']
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            'grails-app/views/error.gsp',
+            'grails-app/controllers/TestController.groovy'
+    ]
 
-  def author = "Silvio Wangler"
-  def authorEmail = "silvio.wangler@gmail.com"
-  def title = "This plugin contains a builder to easily convert your event into the iCalendar format"
-  def description = '''
+    def author = "Silvio Wangler"
+    def authorEmail = "silvio.wangler@gmail.com"
+    def title = "This plugin contains a builder to easily convert your event into the iCalendar format"
+    def description = '''
 	'''
 
-  // URL to the plugin's documentation
-  def documentation = 'http://grails.org/ICalendar+Plugin'
+    // URL to the plugin's documentation
+    def documentation = 'http://grails.org/ICalendar+Plugin'
 
-  def doWithSpring = {
-    // do nothing yet
-  }
-
-  def doWithApplicationContext = {applicationContext ->
-    // TODO Implement post initialization spring config (optional)
-  }
-
-  def doWithWebDescriptor = {xml ->
-    // TODO Implement additions to web.xml (optional)
-  }
-
-  def doWithDynamicMethods = {ctx ->
-
-    // hooking into render method
-    application.controllerClasses.each() {controllerClass ->
-      replaceRenderMethod(controllerClass)
+    def doWithSpring = {
+        // do nothing yet
     }
-  }
 
-  def onChange = {event ->
-
-    // only process controller classes
-    if (application.isArtefactOfType(DefaultGrailsControllerClass.CONTROLLER, event.source)) {
-      def clazz = application.getControllerClass(event.source?.name)
-      replaceRenderMethod(clazz)
+    def doWithApplicationContext = {applicationContext ->
+        // TODO Implement post initialization spring config (optional)
     }
-  }
 
-  def onConfigChange = {event ->
-    // TODO Implement code that is executed when the project configuration changes.
-    // The event is the same as for 'onChange'.
-  }
-
-  /**
-   * This implementation is based on Marc Palmers feed plugin. It hooks into the render method
-   * of a Grails controller class and adds an alternative behaviour for the mime type
-   * 'text/calendar' used by the iCalendar plugin.
-   */
-  private void replaceRenderMethod(controllerClass) {
-
-    println "Modifying render method on controller '${controllerClass.name}'"
-
-    def oldRender = controllerClass.metaClass.pickMethod("render", [Map, Closure] as Class[])
-
-    controllerClass.metaClass.render = {Map params, Closure closure ->
-
-      if (params.contentType?.toLowerCase() == 'text/calendar') {
-
-        response.contentType = 'text/calendar'
-        response.characterEncoding = 'UTF-8'
-        response.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate') //HTTP/1.1
-        response.setHeader('Pragma', 'no-cache') // HTTP/1.0
-
-        def builder = new ICalendarBuilder()
-        builder.invokeMethod('translate', closure)
-
-        render builder.toString()
-
-      } else {
-        // Defer to original render method
-        oldRender.invoke(delegate, [params, closure] as Object[])
-      }
+    def doWithWebDescriptor = {xml ->
+        // TODO Implement additions to web.xml (optional)
     }
-  }
+
+    def doWithDynamicMethods = {ctx ->
+
+        // hooking into render method
+        application.controllerClasses.each() {controllerClass ->
+            replaceRenderMethod(controllerClass)
+        }
+    }
+
+    def onChange = {event ->
+
+        // only process controller classes
+        if (application.isArtefactOfType(DefaultGrailsControllerClass.CONTROLLER, event.source)) {
+            def clazz = application.getControllerClass(event.source?.name)
+            replaceRenderMethod(clazz)
+        }
+    }
+
+    def onConfigChange = {event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    /**
+     * This implementation is based on Marc Palmers feed plugin. It hooks into the render method
+     * of a Grails controller class and adds an alternative behaviour for the mime type
+     * 'text/calendar' used by the iCalendar plugin.
+     */
+    private void replaceRenderMethod(controllerClass) {
+
+        println "Modifying render method on controller '${controllerClass.name}'"
+
+        def oldRender = controllerClass.metaClass.pickMethod("render", [Map, Closure] as Class[])
+
+        controllerClass.metaClass.render = {Map params, Closure closure ->
+
+            if (params.contentType?.toLowerCase() == 'text/calendar') {
+
+                response.contentType = 'text/calendar'
+                response.characterEncoding = 'UTF-8'
+                response.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate') //HTTP/1.1
+                response.setHeader('Pragma', 'no-cache') // HTTP/1.0
+
+                def builder = new ICalendarBuilder()
+                builder.invokeMethod('translate', closure)
+
+                render builder.toString()
+
+            } else {
+                // Defer to original render method
+                oldRender.invoke(delegate, [params, closure] as Object[])
+            }
+        }
+    }
 }
