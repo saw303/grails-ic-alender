47a4cbf25a5c941d36bc4e3d8f5353d79a3a77ec
Silvio Wangler <silvio.wangler@gmail.com>
2014-02-17 13:09:18 +0100

    Upgraded to Grails 2.3.5 and removed 'workaround dependency' backport-util-concurrent since ical4j declares it in current version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,6 +5,21 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -12,12 +27,9 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
         grailsCentral()
-        grailsRepo "http://grails.org/plugins"
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
+        mavenLocal()
         mavenCentral()
         //mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
@@ -28,17 +40,12 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         compile 'org.mnode.ical4j:ical4j:1.0.5.2'
-
-        // due to a bug (http://jira.grails.org/browse/GPICALENDAR-7) a transient dependency of iCal4j needs to be defined as
-        // direct dependency
-        runtime 'backport-util-concurrent:backport-util-concurrent:3.1'
-
         // runtime 'mysql:mysql-connector-java:5.1.5'
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-                ":release:2.2.1",
+        build(":tomcat:7.0.50.1",
+                ":release:3.0.1",
                 ":rest-client-builder:1.0.3") {
             export = false
         }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -17,3 +17,27 @@ grails.doc.subtitle = 'The Grails way of exporting appointments to your users.'
 grails.doc.authors = 'Silvio Wangler'
 grails.doc.license = 'Apache 2'
 grails.doc.copyright = "Copyright 2009 - ${new Date().format('yyyy')} Silvio Wangler"
+
+// Uncomment and edit the following lines to start using Grails encoding & escaping improvements
+
+/* remove this line 
+// GSP settings
+grails {
+    views {
+        gsp {
+            encoding = 'UTF-8'
+            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+            codecs {
+                expression = 'html' // escapes values inside null
+                scriptlet = 'none' // escapes output from scriptlets in GSPs
+                taglib = 'none' // escapes output from taglibs
+                staticparts = 'none' // escapes output from static template parts
+            }
+        }
+        // escapes all not-encoded output at final stage of outputting
+        filteringCodecForContentType {
+            //'text/html' = 'html'
+        }
+    }
+}
+remove this line */
